a124c7ae6f31d0c726b1e8d8ba0bbadc82621682
aniketpange37 <93865898+aniketpange37@users.noreply.github.com>
2021-11-20 16:47:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// error handler modified
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
